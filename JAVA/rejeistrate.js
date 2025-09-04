d42f1337ea6c01533332dcf6a30e385f6613ac70
MartinR842 <pablomartinrios14@abc.gob.ar>
2025-09-04 15:18:13 -0300

    ahora

--- JAVA/rejeistrate.js
+++ JAVA/rejeistrate.js
@@ -1,37 +1,39 @@
-const form = document.getElementById('miFormulario');
-
-  
-
-    const nombre = document.getElementById('nombre').value.trim();
-    if (nombre === '') error;
-
-    const apellido = document.getElementById('apellido').value.trim();
-    if (apellido === '') error;
-
-    const dni = document.getElementById('dni').value.trim();
-    if (dni === '' || isNaN(dni)) error;
-
-    const mail = document.getElementById('mail').value.trim();
-    if (mail === '' || !mail.includes('@')) error;
-
-    const pass = document.getElementById('pass').value.trim();
-    if (pass.length < 6) error;
-
-    const fecha = document.getElementById('date').value;
-    if (fecha === '') error;
-
-    
-  function error(){iziToast.error({
-    title: 'Error',
-    message: errores.join('<br>'),
-    position: 'topRight',
-    timeout: 5000
-  });}
-    
-  iziToast.success({
-    title: 'OK',
-    message: 'Los datos ingresados son correctos!',
-    position: 'topRight'
+const nombre=document.getElementById('nombre').value();
+if (nombre === '') {iziToast.error({
+  title: 'Error',
+  message: 'Este campo es obligatorio'.join('<br>'),
+  position: 'topRight',
+  timeout: 5000
   });
+  };
 
- 
+const apellido = document.getElementById('apellido').value.trim();
+if (apellido === '') {
+  iziToast.error({
+  title: 'Error',
+  message: 'Este campo es obligatorio'.join('<br>'),
+  position: 'topRight',
+  timeout: 5000
+});
+}
+
+const dni = document.getElementById('dni').value.trim();
+if (dni === '' || isNaN(dni)) {
+  iziToast.error({
+  title: 'Error',
+  message: 'Este campo es obligatorio'.join('<br>'),
+  position: 'topRight',
+  timeout: 5000
+});
+}
+
+const mail = document.getElementById('mail').value.trim();
+if (mail === '' || !mail.includes('@')) {
+  iziToast.error({
+  title: 'Error',
+  message: 'Este campo es obligatorio'.join('<br>'),
+  position: 'topRight',
+  timeout: 5000
+});
+};
+
