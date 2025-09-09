b2697edae82ed6f5c68a6685706612e42c664b7d
MartinR842 <pablomartinrios14@abc.gob.ar>
2025-09-09 17:11:14 -0300

    9 de sep

--- JAVA/rejeistrate.js
+++ JAVA/rejeistrate.js
@@ -1,39 +1,110 @@
-const nombre=document.getElementById('nombre').value();
-if (nombre === '') {iziToast.error({
-  title: 'Error',
-  message: 'Este campo es obligatorio'.join('<br>'),
-  position: 'topRight',
-  timeout: 5000
+const btnEnviar=document.getElementById('btnEnviar');
+
+btnEnviar.addEventListener('submit',function(event){
+  event.preventDefault();
+
+  const nombre = document.getElementById('nombre').value.trim();
+  const apellido = document.getElementById('apellido').value.trim();
+  const dni = document.getElementById('dni').value.trim();
+  const mail = document.getElementById('mail').value.trim();
+
+  if (nombre===''){
+      iziToast.error({
+      title: 'Error',
+      message: 'Complete todos los campos',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+      return;
+    
+  }else{
+      iziToast.success({
+      title: 'OK',
+      message: 'Los datos han sido enviados correctamente!',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+
+  }
+  
+
+  if (apellido===''){
+      iziToast.error({
+      title: 'Error',
+      message: 'Complete todos los campos',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+    
+  }else{
+      iziToast.success({
+      title: 'OK',
+      message: 'Los datos han sido enviados correctamente!',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+
+  }
+  
+  if (dni.length=== 8){
+    iziToast.success({
+      title: 'OK',
+      message: 'Los datos han sido enviados correctamente!',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+      
+    
+  }else{
+      
+      iziToast.error({
+      title: 'Error',
+      message: 'Complete todos los campos',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+
+  }
+
+  if (mail.includes('@')){
+    iziToast.success({
+      title: 'OK',
+      message: 'Los datos han sido enviados correctamente!',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+      
+    
+  }else{
+      
+      iziToast.error({
+      title: 'Error',
+      message: 'Complete todos los campos',
+      position:'topCenter',
+      timeout:3000,
+      progressBarColor: 'rgba(19, 182, 136, 1)',
+      TransitionIn:'fadeInDown',
+      });
+
+  }
+  
   });
-  };
 
-const apellido = document.getElementById('apellido').value.trim();
-if (apellido === '') {
-  iziToast.error({
-  title: 'Error',
-  message: 'Este campo es obligatorio'.join('<br>'),
-  position: 'topRight',
-  timeout: 5000
-});
-}
 
-const dni = document.getElementById('dni').value.trim();
-if (dni === '' || isNaN(dni)) {
-  iziToast.error({
-  title: 'Error',
-  message: 'Este campo es obligatorio'.join('<br>'),
-  position: 'topRight',
-  timeout: 5000
-});
-}
 
-const mail = document.getElementById('mail').value.trim();
-if (mail === '' || !mail.includes('@')) {
-  iziToast.error({
-  title: 'Error',
-  message: 'Este campo es obligatorio'.join('<br>'),
-  position: 'topRight',
-  timeout: 5000
-});
-};
 
+  
